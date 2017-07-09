ec1aafdbfccbe3a333d403564c794abac0fdc184
Kan <nemanja.tod@live.com>
2017-07-09 20:34:45 +0200

    Kan's test checkin

--- src/Ondo.Logging.Abstractions/ILoggerAdapter.cs
+++ src/Ondo.Logging.Abstractions/ILoggerAdapter.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace Ondo.Logging.Abstractions
 {
+    /// <summary>
+    /// Kan Test
+    /// </summary>
     public interface ILoggerAdapter: IDisposable
     {
         void AddLog(ILoggerFactory loggerFactory, LoggingOptions loggingOptions);
